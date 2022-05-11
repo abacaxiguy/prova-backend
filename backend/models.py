cdbbc62a87aef2afb8b5cd5d124d615be58a497b
João Lucas <jlucasoc@hotmail.com>
2022-05-10 21:12:18 -0300

    First models implementations ⚙️

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,45 @@
+from pyexpat import model
 from django.db import models
+from django.forms import CharField
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Conta(models.Model):
+    tp_conta = models.CharField(max_length=30)
+    id_banco = models.IntegerField()
+    banco = models.CharField(max_length=50)
+    conta = models.IntegerField()
+    agencia = models.IntegerField()
+    operacao = models.IntegerField()
+
+
+class Uf(models.Model):
+    nome = models.CharField(max_length=30)
+    sigla = models.CharField(max_length=2)
+
+
+class Cidade(models.Model):
+    nome = models.CharField(max_length=50)
+    id_uf = models.ForeignKey(Uf, on_delete=models.PROTECT)
+
+
+
+class Endereco(models.Model):
+    logradouro = models.CharField(max_length=150)
+    numero = models.CharField(max_length=8)
+    cep = models.CharField(max_length=10)
+    bairro = models.CharField(max_length=80)
+    complemento = models.CharField(max_length=60)
+    observacoes = models.TextField()
+    id_cidade = models.ForeignKey(Cidade, on_delete=models.PROTECT)
+
+
+
+class Pessoa(models.Model):
+    vinculo = models.CharField(max_length=20)
+    cpf = models.IntegerField()
+    nome = models.CharField(max_length=200)
+    telefone = models.CharField(max_length=16)
+    email = models.EmailField()
+    id_user = models.OneToOneField(User)
+
---
+++ backend/urls.py
@@ -0,0 +1,6 @@
+from django.urls import include, path
+
+app_name = 'backend'
+urlpatterns = [
+    path('', include())
+]
